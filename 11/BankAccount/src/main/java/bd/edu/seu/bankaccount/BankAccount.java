bf96244e5d974e7e446c86d005ead604d7eecd29
Monirul Hasan <kmhasan@gmail.com>
2020-02-18 14:11:06 +0600

    lecture 5

---
+++ 11/BankAccount/src/main/java/bd/edu/seu/bankaccount/BankAccount.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.edu.seu.bankaccount;
+
+/**
+ *
+ * @author seu
+ */
+public class BankAccount {
+    private long number;
+    private String name;
+    private double balance;
+    
+    public BankAccount(long number, String name, double balance) {
+        this.number = number;
+        this.name = name;
+        this.balance = balance;
+    }
+    
+    public void deposit(double amount) {
+        if (amount >= 0)
+            balance = balance + amount;
+    }
+    
+    public void withdraw(double amount) {
+        if (amount >= 0)
+            balance = balance - amount;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+}
+
+
+
+
+
+
+
+
---
+++ 11/BankAccount/src/test/java/bd/edu/seu/bankaccount/BankAccountTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd.edu.seu.bankaccount;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author seu
+ */
+public class BankAccountTest {
+    
+    public BankAccountTest() {
+    }
+
+    @Test
+    public void testDeposit() {
+        BankAccount bankAccount = new BankAccount(123, "John Doe", 1000);
+        bankAccount.deposit(100);
+        Assertions.assertEquals(1100, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testDepositNegativeAmount() {
+        BankAccount bankAccount = new BankAccount(123, "John Doe", 1000);
+        bankAccount.deposit(-100);
+        Assertions.assertEquals(1000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() {
+        BankAccount bankAccount = new BankAccount(123, "John Doe", 1000);
+        bankAccount.withdraw(100);
+        Assertions.assertEquals(900, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testWithdrawNegativeAmount() {
+        BankAccount bankAccount = new BankAccount(123, "John Doe", 1000);
+        bankAccount.withdraw(-100);
+        Assertions.assertEquals(1000, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testGetBalance() {
+        BankAccount bankAccount = new BankAccount(123, "John Doe", 1000);
+        Assertions.assertEquals(1000, bankAccount.getBalance());
+    }
+    
+}
