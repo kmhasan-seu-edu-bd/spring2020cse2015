80faf052c240ea4cc779c41e575987f412f26fc0
Monirul Hasan <kmhasan@gmail.com>
2020-02-27 14:12:00 +0600

    calculator

--- 12/basic-calculator/src/main/java/bd/edu/seu/CalculatorController.java
+++ 12/basic-calculator/src/main/java/bd/edu/seu/CalculatorController.java
@@ -15,19 +15,6 @@ public class CalculatorController {
     }
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
-
-    @FXML // annotation
-    private void printText() {
-//        String text = displayField.getText();
-//        System.out.printf("Hello, %s!", text);
-        String text = "Papia";
-        displayField.setText(text);
-    }
-
-    @FXML
     private void handleOneAction() {
         String oldText = displayField.getText();
         String newText = oldText + "1";
