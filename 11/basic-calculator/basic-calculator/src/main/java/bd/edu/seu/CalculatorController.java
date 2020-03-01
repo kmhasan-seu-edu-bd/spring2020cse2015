64e70088c2b63cacf4b58c517744bc6730dc3863
Monirul Hasan <kmhasan@gmail.com>
2020-03-01 15:11:19 +0600

    lecture 8

--- 11/basic-calculator/basic-calculator/src/main/java/bd/edu/seu/CalculatorController.java
+++ 11/basic-calculator/basic-calculator/src/main/java/bd/edu/seu/CalculatorController.java
@@ -6,6 +6,10 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 
 public class CalculatorController {
+    private double number1;
+    private double number2;
+    private String operation;
+
     @FXML
     private TextField displayField;
 
@@ -25,5 +29,74 @@ public class CalculatorController {
         String oldText = displayField.getText();
         String newText = oldText + "2";
         displayField.setText(newText);
+    }
+
+    @FXML
+    private void handleThreeAction() {
+        String oldText = displayField.getText();
+        String newText = oldText + "3";
+        displayField.setText(newText);
+    }
+
+    @FXML
+    private void handleFourAction() {
+        String oldText = displayField.getText();
+        String newText = oldText + "4";
+        displayField.setText(newText);
+    }
+
+    @FXML
+    private void handleFiveAction() {
+        String oldText = displayField.getText();
+        String newText = oldText + "5";
+        displayField.setText(newText);
+    }
+
+    @FXML
+    private void handleAddAction() {
+        // 1) copy the current string as a number and save it to number1
+        String oldText = displayField.getText();
+        number1 = Double.parseDouble(oldText);
+
+        // 2) clear the display field
+        displayField.clear();
+
+        // 3) remember that it's an add action
+        operation = "ADD";
+    }
+
+    @FXML
+    private void handleSubtractAction() {
+        // 1) copy the current string as a number and save it to number1
+        String oldText = displayField.getText();
+        number1 = Double.parseDouble(oldText);
+
+        // 2) clear the display field
+        displayField.clear();
+
+        // 3) remember that it's an subtract action
+        operation = "SUBTRACT";
+    }
+
+    @FXML
+    private void handleEqualAction() {
+        // 1) copy the current string as a number and save it to number2
+        String oldText = displayField.getText();
+        number2 = Double.parseDouble(oldText);
+
+        // 2) add/subtract number1 and number2 and find the result
+        double result = 0;
+        switch (operation) {
+            case "ADD":
+                result = number1 + number2;
+                break;
+
+            case "SUBTRACT":
+                result = number1 - number2;
+                break;
+        }
+
+        // 3) display the result as a string in the displayField
+        displayField.setText(String.format("%.2f", result));
     }
 }
--- 12/basic-calculator/src/main/java/bd/edu/seu/CalculatorController.java
+++ 12/basic-calculator/src/main/java/bd/edu/seu/CalculatorController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 
 public class CalculatorController {
+    private double number1;
+    private double number2;
+    private String operation;
 
     @FXML
     private TextField displayField;
@@ -20,6 +23,7 @@ public class CalculatorController {
         String newText = oldText + "1";
         displayField.setText(newText);
     }
+
     @FXML
     private void handleTwoAction() {
         String oldText = displayField.getText();
@@ -33,4 +37,59 @@ public class CalculatorController {
         String newText = oldText + "3";
         displayField.setText(newText);
     }
+
+    @FXML
+    private void handleAddAction() {
+        // 1) copy the current string as a number and save
+        // it to number1
+        String oldText = displayField.getText();
+        number1 = Double.parseDouble(oldText);
+
+        // 2) clear the displayField
+        displayField.clear();
+
+        // 3) remember that we are supposed to add
+        operation = "ADD";
+    }
+
+    @FXML
+    private void handleSubtractAction() {
+        // 1) copy the current string as a number and save
+        // it to number1
+        String oldText = displayField.getText();
+        number1 = Double.parseDouble(oldText);
+
+        // 2) clear the displayField
+        displayField.clear();
+
+        // 3) remember that we are supposed to subtract
+        operation = "SUBTRACT";
+    }
+
+    @FXML
+    private void handleEqualAction() {
+        // 1) copy the current string as a number to number2
+        String oldText = displayField.getText();
+        number2 = Double.parseDouble(oldText);
+
+        // 2) add number1 and number2
+        double result = 0;
+        switch (operation) {
+            case "ADD":
+                result = number1 + number2;
+                break;
+            case "SUBTRACT":
+                result = number1 - number2;
+                break;
+        }
+        //number1 + number2;
+
+        // 3) display the result as a String
+        String newText = String.format("%.2f", result);
+        displayField.setText(newText);
+    }
 }
+
+
+
+
